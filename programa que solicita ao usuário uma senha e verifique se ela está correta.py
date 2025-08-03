d1c6e0bbcd43ec18779e8b4f6605584131e9b05c
Anderson Parra | Computadores e Sistemas® <38863847+parrAnderson@users.noreply.github.com>
2025-08-03 17:53:06 -0300

    Add files via upload
    programa que  Solicita senha ao usuário e verifique se ela está correta

---
+++ programa que solicita ao usuário uma senha e verifique se ela está correta.py
@@ -0,0 +1,8 @@
+#Escreva um programa que peça ao usuário uma senha e verifique se ela está correta (a senha correta é "python123").
+
+senha= input('Digite a senha')
+
+if senha== 'python123':
+    print('Senha correta')
+
+else: print('Senha Incorreta')
